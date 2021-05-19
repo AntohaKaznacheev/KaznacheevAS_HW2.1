5abe7eced515cfd5cb7da9552eeb47f5a82a3348
Kaznacheev <antkaznacheev@gmail.ru>
2021-05-19 07:51:38 +0300

    Initial Commit

---
+++ KaznacheevAS_HW2.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KaznacheevAS_HW2.1
+//
+//  Created by Kaznacheev on 19.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
